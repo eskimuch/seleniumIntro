eb1ba689102b8abf04c2a714948d9220b86ef4e4
eskimuch <eskimuch@gmail.com>
2019-03-02 20:05:27 +0100

    ScreenShot @Attachment added

---
+++ src/test/java/jpetstore/utils/ScreenShotMaker.java
@@ -0,0 +1,16 @@
+package jpetstore.utils;
+
+import io.qameta.allure.Attachment;
+import jpetstore.driver.manager.DriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class ScreenShotMaker {
+
+    @Attachment(value = "Page screenshot test failure", type = "image/png")
+    public static byte[] makeScreenShot() {
+        byte[] screenshotFile = ((TakesScreenshot) DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+        return screenshotFile;
+    }
+
+}
